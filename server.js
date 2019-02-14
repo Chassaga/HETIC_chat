801abb5f28c16081fb956a17191a7029091e49a9
Antoine Chassagnoux <antoinechassagnoux@MacBook-Pro-de-Antoine-8.local>
2019-02-14 11:33:19 +0100

    ajout de npm install

--- server.js
+++ server.js
@@ -1,5 +1,39 @@
 /* Imports */
 
+    // => Variables d'environnemnt
+
+
+
+    // => Créer des constantes pour importer les modules NPM
+
+
+
+    // => Configuration
+
+
+
+    // => Routeurs
+
 /* Configuration */
 
+    // => Configuration de base
+
+
+
+    // => Configuration du DOSSIER client
+
+
+
+    // => Configuration du moteur de rendu
+
+
+
+    // => Configuration des routes
+
 /* Lancements pour démarrer le serveur */
+
+    // => Configuration de la bdd MongoDB
+
+
+
+    // =>
